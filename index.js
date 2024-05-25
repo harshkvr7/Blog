c72f9ef202f0e785c3476a690598ff1ae6c5e8f2
Harsh <harshkvr7@gmail.com>
2024-05-26 01:02:18 +0530

    added pictures

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import axios from "axios";
 import bodyparser from "body-parser";
 import pg from "pg";
+import fileupload from "express-fileupload";
+import { Buffer } from "buffer"
 
 function get_date_time() {
     var dateraw = new Date();
@@ -91,24 +93,25 @@ const port = process.env.PORT || 4000;
 
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: true }));
+app.use(fileupload());
 
 const db = new pg.Client({
-    user : "postgres.dgaaajenixemzvusrpel",
-    host : "aws-0-ap-south-1.pooler.supabase.com",
-    database : "postgres",
-    password : "Harshkvr@2005",
-    post : 5432,
+    user: "postgres.dgaaajenixemzvusrpel",
+    host: "aws-0-ap-south-1.pooler.supabase.com",
+    database: "postgres",
+    password: "Harshkvr@2005",
+    post: 5432,
 })
 
 db.connect();
 
 var data;
 
-db.query("SELECT * FROM blogs ORDER BY id desc LIMIT 10 OFFSET ($1 - 1) * 10",[1], (err, res) => {
+db.query("SELECT * FROM blogs ORDER BY id desc LIMIT 10 OFFSET ($1 - 1) * 10", [1], (err, res) => {
     if (err) {
         console.error("error executing query", err.stack);
     }
-    else{
+    else {
         data = res.rows;
     }
 });
@@ -118,7 +121,7 @@ app.get("/", async (req, res) => {
     const blogCountResult = await db.query("SELECT COUNT(id) as count from blogs;");
     const blogCount = blogCountResult.rows[0].count;
 
-    var page = req.query.pg || 1; 
+    var page = req.query.pg || 1;
 
     console.log(page);
 
@@ -142,15 +145,28 @@ app.get("/addblog", (req, res) => {
     res.render("./edit.ejs");
 })
 
-app.post("/addblog",async (req, res) => {
+app.post("/addblog", async (req, res) => {
     var date_time = get_date_time();
 
-    await db.query("INSERT INTO blogs (content, date, month) VALUES ($1, $2, $3)",[req.body.text, date_time.date, date_time.month], () => {
-        res.redirect("/");
-    });
+
+    if (req.files) {
+        const { name, data } = req.files.pic;
+
+        const base64 = Buffer.from(data,
+            "binary").toString("base64");
+
+        await db.query("INSERT INTO blogs (content, date, month, img) VALUES ($1, $2, $3, $4)", [req.body.text, date_time.date, date_time.month, base64], () => {
+            res.redirect("/");
+        });
+    }
+    else {
+        await db.query("INSERT INTO blogs (content, date, month) VALUES ($1, $2, $3)", [req.body.text, date_time.date, date_time.month], () => {
+            res.redirect("/");
+        });
+    }
 })
 
-app.get("/deleteblog",async (req, res) => {
+app.get("/deleteblog", async (req, res) => {
     const blogId = req.query.id;
 
     await db.query("DELETE FROM blogs WHERE id = $1", [blogId], () => {
@@ -164,11 +180,10 @@ app.get("/updateblog", async (req, res) => {
     const result = await db.query("SELECT * FROM blogs WHERE id = $1", [blogId]);
     const blog = result.rows[0];
 
-    console.log(blog);
-    res.render("./edit.ejs", { blog : blog });
+    res.render("./edit.ejs", { blog: blog });
 });
 
-app.post("/updateblog",async (req, res) => {
+app.post("/updateblog", async (req, res) => {
     const { id, content } = req.body;
 
     await db.query("UPDATE blogs SET content = $1 WHERE id = $2", [content, id], () => {
